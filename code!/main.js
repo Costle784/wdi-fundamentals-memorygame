9ed41e534d8832b64dae2adaafe37ebfc749238c
Curtis Ostle <curtisostle@gmail.com>
2017-01-21 16:43:53 -0500

    css reset

--- code!/main.js
+++ code!/main.js
@@ -1,21 +1,78 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
 
-document.querySelector('.board').setAttribute('id','game-board');
+document.querySelector('.board').setAttribute('id','game-board'); //set id (game-board) for wrapper (div) to hold new cardElements. 
+var gameBoard = document.getElementById('game-board');			//set this to variable gameBoard.
 
-function createCards(){
-	for(var i = 1; i <= 4;i++){
-	var gameBoard = document.getElementById('game-board');
-	var newDiv = document.createElement('div');
-	newDiv.className="card";
-	gameBoard.appendChild(newDiv);
+var cardsInPlay = []; 
+var cards = [];
+
+
+function createCardArray(cardNum){		//loop to create new cardElents (divs), append them as children to gameBoard.
+	debugger;							//assign each cardElement a class of 'card' as a selector for css. 
+										//push cardElements into an array, var cards. 		
+	for(var i = 0; i < cardNum; i++){
+		var cardElement = document.createElement('div');
+		cardElement.className='card';
+		gameBoard.appendChild(cardElement);
+		cards.push(cardElement); 
+	}
+}
+createCardArray(4);
+
+function setAttributes(cardValueArray){
+	debugger;
+	for(var i = 0; i < cards.length; i++){	
+		cards[i].addEventListener('click', flipOver)  // add 'click' event listener to each card and have it trigger flipOver function
+		cards[i].setAttribute('data-card', cardValueArray[i]); // add 'data-card' attribute and value to each card
+	}	
+}
+setAttributes(["queen","queen","king","king"]);
+
+
+
+function flipOver(){
+	if(this.getAttribute('data-card') === "queen"){
+		this.innerHTML = '<img src="images/Queen.jpg">';
+		cardsInPlay.push(this.getAttribute('data-card'));
+		if(cardsInPlay.length === 2){
+			isMatch();
+		}
+	}
+	else{
+		this.innerHTML = '<img src="images/King.jpg">';
+		cardsInPlay.push(this.getAttribute('data-card'));
+		if(cardsInPlay.length === 2){
+			isMatch();
+		}
 	}
 }
 
-createCards(); 
+function isMatch(){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("Match!");	
+		cardsInPlay = [];
+	}	
+}
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
