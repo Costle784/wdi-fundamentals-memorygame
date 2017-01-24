81eac22104c1375acbad678961f83e96dc68ba54
Curtis Ostle <curtisostle@gmail.com>
2017-01-23 20:50:31 -0500

    shuffle cards function

--- code!/main.js
+++ code!/main.js
@@ -9,7 +9,7 @@ var cards = [];
 
 
 function createCardArray(cardNum){		//loop to create new cardElents (divs), append them as children to gameBoard.
-	debugger;							//assign each cardElement a class of 'card' as a selector for css. 
+						//assign each cardElement a class of 'card' as a selector for css. 
 										//push cardElements into an array, var cards. 		
 	for(var i = 0; i < cardNum; i++){
 		var cardElement = document.createElement('div');
@@ -18,18 +18,32 @@ function createCardArray(cardNum){		//loop to create new cardElents (divs), appe
 		cards.push(cardElement); 
 	}
 }
-createCardArray(4);
+createCardArray(8);
 
-function setAttributes(cardValueArray){
-	debugger;
+var valueArray = ["queen","queen","king","king","queen","queen","king","king"]
+
+function shuffle(array) {
+  var m = array.length, t, i;
+  
+  	while(m){
+	    i = Math.floor(Math.random() * m--);
+	    t = array[m];
+	    array[m] = array[i];
+	    array[i] = t;
+	}
+  	console.log(valueArray);
+}
+
+shuffle(valueArray);
+
+function setAttributes(array){
+	
 	for(var i = 0; i < cards.length; i++){	
 		cards[i].addEventListener('click', flipOver)  // add 'click' event listener to each card and have it trigger flipOver function
-		cards[i].setAttribute('data-card', cardValueArray[i]); // add 'data-card' attribute and value to each card
+		cards[i].setAttribute('data-card', valueArray[i]); // add 'data-card' attribute and a random 'king' or 'queen' value to 'data-card'
 	}	
 }
-setAttributes(["queen","queen","king","king"]);
-
-
+setAttributes(valueArray);
 
 function flipOver(){
 	if(this.getAttribute('data-card') === "queen"){
@@ -48,13 +62,17 @@ function flipOver(){
 	}
 }
 
-function isMatch(){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("Match!");	
-		cardsInPlay = [];
-	}	
-}
 
+ function isMatch() {
+ 	if(cardsInPlay[0] === cardsInPlay[1]){   
+	var popup = document.querySelector('.popuptext');
+	    popup.classList.toggle('show');
+ 	} 
+ }
+
+
+// var popup = document.getElementById('myPopup');
+//     popup.classList.toggle('show');
 
 
 
