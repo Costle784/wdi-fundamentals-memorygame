635251f50f00379af58101b2e91ebdf912b6f293
Curtis Ostle <curtisostle@gmail.com>
2017-03-10 15:14:52 -0500

    new changes

--- code!/main.js
+++ code!/main.js
@@ -1,29 +1,29 @@
 console.log("JS file is connected to HTML! Woo!");
 
-var cardInput; 
-
-setTimeout(getValue,400);
+setTimeout(getValue,400);  
 
 function showPrompt() {
 	return window.prompt("Welcome to Forget Me Not!\nPlease enter a number 1-5\n1 - simple\n2 - easy\n3 - just right\n4 - hard\n5 - INSANE","3")
 }	
 
+var cardInput;
+
 function getValue() {
-	switch (Number(showPrompt())) {
-		case 5:
+	switch ((showPrompt())) {
+		case '5':
 			cardInput = 24;
 			break;
-		case 4:
+		case '4':
 			cardInput = 18;
 			break;	
-		case 3:
+		case '3':
 			cardInput = 12;
 			break;	
-		case 2:
+		case '2':
 			cardInput = 8;
 			break;
-		case 1:
-			cardInput = 4;
+		case '1':
+			cardInput = 6;
 			break;
 		default:	
 			cardInput = 12;
@@ -32,11 +32,10 @@ function getValue() {
 	makeCards(cardInput);
 }
 
-var cards = [];
-var gameBoard = document.getElementById('game-board');
+var gameBoard = document.getElementById('game-board'), cards = [];
 
-function makeCards(num){		
-	for(var i = 0; i < cardInput; i++){
+function makeCards(num) {		
+	for (var i = 0; i < cardInput; i++){
 		var cardElement = document.createElement('div');
 		cardElement.className ='card'; 
 		gameBoard.appendChild(cardElement);
@@ -57,14 +56,12 @@ function makeValueArray(num) {
 			valueArray.push("king")
 		}
 	}
-
-shuffle(valueArray);
+	shuffle(valueArray);
 } 
 
 function shuffle(arr) {
  	var m = arr.length, t, i;
-  
-  	while(m){
+  	while(m) {
 	    i = Math.floor(Math.random() * m--);
 	    t = arr[m];
 	    arr[m] = arr[i];
@@ -74,52 +71,57 @@ setAttributes(valueArray);
 }
 
 function setAttributes(arr) {
-	for(var i = 0; i < cards.length; i++) {	
-		cards[i].addEventListener('click', flipOver)  // add 'click' event listener to each card and have it trigger flipOver function
-		cards[i].setAttribute('data-card', valueArray[i]); // add 'data-card' attribute and a random 'king' or 'queen' value to 'data-card'
+	for (var i = 0; i < cards.length; i++) {	
+		// if (!(cards[i].getAttribute('data-card'))) {
+			cards[i].addEventListener('click', flipOver)  // add 'click' event listener to each card and have it trigger flipOver function
+			cards[i].setAttribute('data-card', valueArray[i]); // add 'data-card' attribute and a random 'king' or 'queen' value to 'data-card'
+		// }
 	}	
 }
 
 var cardsInPlay = []; 
-
-function flipOver(){
-	if(this.getAttribute('data-card') === "queen"){
+console.log(cardsInPlay);
+function flipOver() {
+	if (this.getAttribute('data-card') === "queen") {
 		this.innerHTML = '<img src="images/Queen.jpg">';
 		cardsInPlay.push(this.getAttribute('data-card'));
-		if(cardsInPlay.length === 2){
-			isMatch();
-		}
+		this.setAttribute('data-card', null);
+		(console.log(cardsInPlay))
 	}
-	else{
-		this.innerHTML = '<img src="images/King.jpg">';
-		cardsInPlay.push(this.getAttribute('data-card'));
-		if(cardsInPlay.length === 2){
-			isMatch();
-		}
+	else if (this.getAttribute('data-card') === "king") {
+			this.innerHTML = '<img src="images/King.jpg">';
+			cardsInPlay.push(this.getAttribute('data-card'));
+			this.setAttribute('data-card', null);
+			(console.log(cardsInPlay))
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch();
 	}
 }
 
+function isMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		var popup = document.getElementById('myPopup');
+		popup.classList.toggle('show');
+		cardsInPlay = [];
+	}	
+	else {
+		window.setTimeout(flipBackOver, 2000);
+		cardsInPlay = [];
+	}	
+}
 
-// function quickDisplay(arr){
-//  	for(i = 0; i < arr.length; i++){
-//  		if(arr[i] === "queen"){
-//  			setTimeout(showCard, 3000);
-// 		}
-//  	}
-// }
-
-function createStartButton(){
+function createStartButton() {
 	var startButton = document.createElement('button');
 	var startContainer = document.getElementById('startContainer');
 	startContainer.appendChild(startButton);
 	startButton.innerHTML = 'Start';
-	startButton.classname = "startbutt";
 	startButton.addEventListener('click', showCards)
 }
 
 
-function showCards(arr){
-	for(i = 0; i < cards.length; i++){
+function showCards(arr) {
+	for (i = 0; i < cards.length; i++) {
 		
 		if(cards[i].getAttribute('data-card') === "queen"){
 			cards[i].innerHTML = '<img src="images/Queen.jpg">';
@@ -140,12 +142,7 @@ function flipBackOver(){
 	}
 }
 
-function isMatch(){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		var popup = document.getElementById('myPopup');
-		popup.classList.toggle('show');
-	}
-}
+
 
 
 
